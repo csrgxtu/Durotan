bf18cf1c4e25b38b104483dde37827cf777c9243
Archer Reilly <1246506786@qq.com>
2016-06-15 16:45:44 +0800

    auth controller

--- controllers/auth.go
+++ controllers/auth.go
@@ -17,27 +17,3 @@ func (this *AuthController) Login() {
   this.Data["json"] = &rt
   this.ServeJSON()
 }
-
-func (this *AuthController) Read() {
-	skip, _ := this.GetInt(":skip")
-	limit, _ := this.GetInt(":limit")
-	var rt models.Result
-
-	err, rtv := services.Read(skip, limit)
-	if err != nil {
-		rt.Error = true
-		rt.Msg = "Failure"
-		rt.Data = make([]models.Recs, 1)
-		rt.Data[0] = err.Error()
-	} else {
-		rt.Error = false
-		rt.Msg = "Successful"
-		rt.Data = make([]models.Recs, len(rtv))
-		for ix, value := range rtv {
-			rt.Data[ix] = value
-		}
-	}
-
-	this.Data["json"] = &rt
-	this.ServeJSON()
-}
