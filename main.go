57067d106e508e1e761cd3111a5751cdacb36ee8
Archer Reilly <1246506786@qq.com>
2016-06-17 15:55:22 +0800

    add cors

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+  "github.com/astaxie/beego/plugins/cors"
+  "github.com/astaxie/beego"
 	_ "Durotan/routers"
-	"github.com/astaxie/beego"
 )
 
 func main() {
+  // CORS for https://foo.* origins, allowing:
+  // - PUT and PATCH methods
+  // - Origin header
+ // // - Credentials share
+  beego.InsertFilter("*", beego.BeforeRouter,cors.Allow(&cors.Options{
+  AllowOrigins: []string{"*"},
+  AllowMethods: []string{"*"},
+  AllowHeaders: []string{"Origin"},
+  ExposeHeaders: []string{"Content-Length"},
+  AllowCredentials: true,
+  }))
+
+  
 	beego.Run()
 }
-
